e49163a1289aaa25658396bcb35cf89f67d30e04
jaxiuhe <jaxiuhe@tencent.com>
2019-01-11 21:02:44 +0800

    修正时间转换失败事例

--- timepro/time.go
+++ timepro/time.go
@@ -1,15 +1,22 @@
 package timepro
 
-import "time"
-import "standAlone/utils/log"
+import (
+	"standAlone/utils/log"
+	"time"
+)
 
 // StringToTime 序列化字符串时间
 func StringToTime(t string) time.Time {
-	withNanos := "2006-01-02 15:04:36"
-	tm, err := time.Parse(withNanos[:len(t)], t)
+	withNanos := "2006-01-02 15:04:05"
+	// var cstZone = time.FixedZone("CST", 8*3600)
+	cstZone, err := time.LoadLocation("Local")
+	if err != nil {
+		return time.Now()
+	}
+	tm, err := time.ParseInLocation(withNanos[:len(t)], t, cstZone)
 	if err != nil {
 		log.CheckErr(err, log.LogHigh)
 		return time.Now()
 	}
-	return tm
+	return tm.Local()
 }
