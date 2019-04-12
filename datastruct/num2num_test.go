d4755cf4781f836886adf962e09a82ea03428497
jaxiuhe <jaxiuhe@tencent.com>
2019-04-12 21:13:56 +0800

    进制转换测试单元的实现

---
+++ datastruct/num2num_test.go
@@ -0,0 +1,10 @@
+package datastruct
+
+import "testing"
+
+func Test_Num2Num(t *testing.T) {
+	t.Log(JinZhiChange(22, 2))
+	t.Log(JinZhiChange(22, 8))
+	t.Log(JinZhiChange(22, 16))
+	t.Log(JinZhiChange(22, 10))
+}
