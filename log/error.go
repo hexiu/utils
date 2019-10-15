63fed5de6c3c2f71d8ae1e6c9d9bcb0aba8d69cb
云凡 <hejiahao@innotechx.com>
2019-10-15 15:02:01 +0800

    取消路径打印

--- log/error.go
+++ log/error.go
@@ -1,9 +1,9 @@
 package log
 
 import (
-	"log"
-	"os"
-	"path/filepath"
+        "log"
+        "os"
+        "path/filepath"
 )
 
 const logPath = "logs"
@@ -20,51 +20,53 @@ const logflag = 5
 var logger *log.Logger
 
 func init() {
-	initlog()
+        initlog()
 }
 
 func JudPathExist(path string) bool {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true
-	}
-	if os.IsNotExist(err) {
-		return false
-	}
-	return false
+        _, err := os.Stat(path)
+        if err == nil {
+                return true
+        }
+        if os.IsNotExist(err) {
+                return false
+        }
+        return false
 }
 
 func initlog() {
-	PrintAbsPath()
-	pathexist := JudPathExist(logPath)
-	if pathexist != true {
-		os.Mkdir(logPath, os.ModePerm)
-	}
-	filename := filepath.Join(logPath, logFile)
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
-	if err != nil {
-		log.Panic("create log file error! info:", err)
-	}
-	logger = log.New(file, "[StandAlone]", logflag)
+        //      PrintAbsPath()
+        pathexist := JudPathExist(logPath)
+        if pathexist != true {
+                os.Mkdir(logPath, os.ModePerm)
+        }
+        filename := filepath.Join(logPath, logFile)
+        file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+        if err != nil {
+                log.Panic("create log file error! info:", err)
+        }
+        logger = log.New(file, "[StandAlone]", logflag)
 }
 
 func PrintAbsPath() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(dir)
+        dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+        if err != nil {
+                log.Fatal(err)
+        }
+        log.Println(dir)
 }
 
 func CheckErr(err error, logtype int) {
-	if err != nil {
-		logger.Println(err)
-		switch logtype {
-		case LogHigh:
-			panic(err)
-		default:
-			break
-		}
-	}
-	return
+        if err != nil {
+                logger.Println(err)
+                switch logtype {
+                case LogHigh:
+                        panic(err)
+                default:
+                        break
+                }
+        }
+        return
 }
+
+
